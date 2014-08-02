f17d4bd92d3299e521b0a4092c308f23fbdb8571
upamanyu92 <upamanyu92@users.noreply.github.com>
2014-08-02 17:54:17 +0530

    Create ControlledScreen.java

---
+++ ControlledScreen.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package newapplication;
+
+/**
+ *
+ * @author SMART
+ */
+public interface ControlledScreen {
+    
+    public void setScreenParent(ScreenControl screenPage);
+}
